b3c637af324da43120bc1c4b45bc665cdc5d9e66
quykimdoan26-prog <quykimdoan26@gmail.com>
2026-02-06 21:46:25 +0700

    them bai tap 20t

---
+++ Daochunhom.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+        for s in strs:
+        # Sắp xếp ký tự trong chuỗi để tạo key
+            key = ''.join(sorted(s)) # sắp xếp tạo key trùng nhau với các chữ cái là lộn xộn
+            groups[key].append(s) # thêm chuỗi "s" vào danh sách của key. 
+    
+        return list(groups.values()) #trả về một list value của các key là từng phần tử trong list
